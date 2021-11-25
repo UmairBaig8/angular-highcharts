f5a9dd0b70abcf9ded577335c9c7e8a0b838a0b7
UmairBaig8 <umairbaig808@gmail.com>
2021-11-25 16:06:03 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
-import { HighchartsChartComponent } from 'highcharts-angular';
+import { SankeyChartComponent } from './sankey-chart/sankey-chart.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HighchartsChartComponent],
+  imports: [BrowserModule, FormsModule, HighchartsChartModule],
+  declarations: [AppComponent, HelloComponent, SankeyChartComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/sankey-chart/sankey-chart.component.ts
+++ src/app/sankey-chart/sankey-chart.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-sankey-chart',
   templateUrl: './sankey-chart.component.html',
-  styleUrls: ['./sankey-chart.component.css']
+  styleUrls: ['./sankey-chart.component.css'],
 })
 export class SankeyChartComponent implements OnInit {
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
+  chartOptions: Highcharts.Options = {
+    series: [
+      {
+        type: 'sankey',
+        colors: [
+          '#2f7ed8',
+          '#0d233a',
+          '#8bbc21',
+          '#910000',
+          '#1aadce',
+          '#492970',
+          '#f28f43',
+          '#77a1e5',
+          '#c42525',
+          '#a6c96a',
+        ],
+        data: [
+          {
+            from: 'Category1',
+            to: 'Category2',
+            weight: 2,
+          },
+          {
+            from: 'Category1',
+            to: 'Category3',
+            weight: 5,
+          },
+        ],
+      },
+    ],
+  };
+  chartCallback: Highcharts.ChartCallbackFunction = function (chart) {
+    console.log('Chart rendered');
+  }; // optional function, defaults to null
+  updateFlag: boolean = false; // optional boolean
+  oneToOneFlag: boolean = true; // optional boolean, defaults to false
+  runOutsideAngularFlag: boolean = false; // optional boolean, defaults to false
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
